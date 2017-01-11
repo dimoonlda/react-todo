f173f8bb12cd1cabd9cb450393128b866a2b9895
lutay.d <dmytro.lutai@roots.com.ua>
2017-01-11 18:42:58 +0200

    styled TodoApp, search and add components

--- app/components/AddTodoForm.jsx
+++ app/components/AddTodoForm.jsx
@@ -16,7 +16,7 @@ var AddTodoForm = React.createClass({
     },
     render: function () {
         return (
-            <div>
+            <div className="container__footer">
                 <form onSubmit={this.onSubmit}>
                     <input ref="todoText" type="text" placeholder="What do you need to do?"/>
                     <button className="button expanded">Add Todo</button>
--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -8,59 +8,66 @@ var TodoSearch = require('TodoSearch');
 var TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
-  getInitialState: function () {
-    return {
-      showCompleted: false,
-      searchText: '',
-      todos: TodoAPI.getTodos()
-    }
-  },
-  componentDidUpdate: function () {
-    TodoAPI.setTodos(this.state.todos);
-  },
-  handleAddTodo: function (text) {
-    this.setState({
-      todos: [
-          ...this.state.todos,
-        {
-          id: uuid(),
-          text: text,
-          completed: false,
-          createdAt: moment().unix(),
-          completedAt: undefined
+    getInitialState: function () {
+        return {
+            showCompleted: false,
+            searchText: '',
+            todos: TodoAPI.getTodos()
         }
-      ]
-    });
-  },
-  handleSearch: function (showCompleted, searchText) {
-    this.setState({
-      showCompleted: showCompleted,
-      searchText: searchText.toLowerCase()
-    });
-  },
-  handleToggle: function (id) {
-    var updatedTodos = this.state.todos.map((todo) => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed;
-        todo.completedAt = todo.completed ? moment().unix() : undefined;
-      }
-      return todo;
-    });
-    this.setState({
-      todos: updatedTodos
-    });
-  },
-  render: function () {
-    var {todos, showCompleted, searchText} = this.state;
-    var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
-    return (
-      <div>
-        <TodoSearch onSearch={this.handleSearch}/>
-        <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
-        <AddTodoForm onAddTodo={this.handleAddTodo}/>
-      </div>
-    )
-  }
+    },
+    componentDidUpdate: function () {
+        TodoAPI.setTodos(this.state.todos);
+    },
+    handleAddTodo: function (text) {
+        this.setState({
+            todos: [
+                ...this.state.todos,
+                {
+                    id: uuid(),
+                    text: text,
+                    completed: false,
+                    createdAt: moment().unix(),
+                    completedAt: undefined
+                }
+            ]
+        });
+    },
+    handleSearch: function (showCompleted, searchText) {
+        this.setState({
+            showCompleted: showCompleted,
+            searchText: searchText.toLowerCase()
+        });
+    },
+    handleToggle: function (id) {
+        var updatedTodos = this.state.todos.map((todo) => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed;
+                todo.completedAt = todo.completed ? moment().unix() : undefined;
+            }
+            return todo;
+        });
+        this.setState({
+            todos: updatedTodos
+        });
+    },
+    render: function () {
+        var {todos, showCompleted, searchText} = this.state;
+        var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
+        return (
+            <div>
+                <h1 className="page-title">Todo App</h1>
+                <div className="row">
+                    <div className="columns medium-6 large-5 small-centered small-11">
+                        <div className="container">
+                            <TodoSearch onSearch={this.handleSearch}/>
+                            <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
+                            <AddTodoForm onAddTodo={this.handleAddTodo}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 });
 
 module.exports = TodoApp;
